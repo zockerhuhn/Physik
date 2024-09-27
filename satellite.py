fbd013a04cd06d1477390b0772302d67092d8ce8
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-09-27 09:33:42 +0200

    Update satellite.py

--- satellite.py
+++ satellite.py
@@ -36,9 +36,9 @@ def step():
       i.apply_force(z.force * math.cos(angle_radians), z.force * math.sin(angle_radians), deltaT)
 
 satellite.yVelocity = 7660
-for e in range(2500000):
+for e in range(150*600):
   step()
-  if e%100000== 0:
+  if e%1000== 0:
     print(f"x: {satellite.x}, y: {satellite.y}, vx: {satellite.xVelocity}, vy: {satellite.yVelocity}")
   #sleep(0.5)
 fig, axs = plt.subplots(nrows=1, ncols=1)
