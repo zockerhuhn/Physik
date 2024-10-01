d1567210903dcfe7bcbd1dbb27944a9b3ac7be46
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-10-01 13:32:53 +0200

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -4,6 +4,6 @@ satelliteStartingXVelocity = 0
 satelliteStartingYVelocity = 7000
 
 deltaT:float = 0.1
-amountOfSteps:int =3500*600
+amountOfSteps:int =3000*600
 printEveryNthStep:int = 100000
 saveToLogEveryNthStep:int = 100
