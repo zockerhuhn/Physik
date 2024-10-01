a9f5050076e250badf127a370059947eee662072
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-10-01 13:47:00 +0200

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,7 +1,7 @@
-satelliteStartingX:float = 7500000#+6.371e6
+satelliteStartingX:float = 400000+6.371e6 #7500000
 satelliteStartingY:float = 0
 satelliteStartingXVelocity = 0
-satelliteStartingYVelocity = 9000
+satelliteStartingYVelocity = 7660
 
 deltaT:float = 0.1
 amountOfSteps:int =2900*600
